ae5df4b2448faa9c571b745f3aa1da5fb389e746
swortiz <ortizsteven61@gmail.com>
2025-10-31 16:06:03 -0700

    Lab 9 completed still issues with the clicking on pokemon and icons

---
+++ packages/proto/src/pkm-list.ts
@@ -0,0 +1,58 @@
+import {html, css, LitElement} from "lit";
+import {property, state} from "lit/decorators.js";
+import "./pkm-card";
+
+interace Card{
+	name: string;
+	type: string;
+	rarity: string;
+	imgSrc: string;
+	link: string;
+}
+
+export class PkmListElement extends LitElement {
+
+	@property() src?: string;
+	@state() cards: Array<Card> = [];
+
+	connectedCallback(){
+		super.connectedCallback();
+		if(this.src) this.hydrate(this.src);
+	}
+	hydrate(src: string){
+		fetch(src)
+			.then((res) => res.json())
+			.then((json: Array<Card>) => {
+				this.cards = json;
+			})
+			.catch((err) => console.error("Error loading the data:", err));
+	}
+	render(){
+		return html`
+		<ul>
+		  ${this.cards.map(
+			(c) => html`
+			  <li>
+				<pkm-card
+				  img-src=${c.imgSrc}
+				  href=${c.link}
+				  name=${c.name}
+				  type=${c.type}
+				  rarity=${c.rarity}
+				></pkm-card>
+			  </li>
+		  `
+		  )}
+		</ul>
+	  `;
+	}
+	static styles = css`
+	  ul {
+		list-style: none;
+		padding: 0;
+	  }
+	  li {
+	  	margin: 0.5rem 0;
+	  }
+	`;
+}
