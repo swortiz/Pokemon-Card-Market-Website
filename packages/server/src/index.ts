c13d98c7602967a40ccbf0804312bd63c35d8cca
swortiz <ortizsteven61@gmail.com>
2025-11-10 13:01:42 -0800

    halfway through lab 10

---
+++ packages/server/src/index.ts
@@ -0,0 +1,19 @@
+// add this import near the top
+import { connect } from "./services/mongo";
+
+connect("Pokemon");
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+const staticDir = process.env.STATIC || "public";
+
+app.use(express.static(staticDir));
+
+app.get("/hello", (req: Request, res: Response) => {
+    res.send("Hello, World");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
